0253dce5bec93df7ded960878676569eda7b9b17
Archit Kashyap <Archit.Kashyap@infobeans.in>
2018-07-23 14:53:25 +0530

    All four Forms of binding

---
+++ data-binding-demo/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
+import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StringInterpolationComponent,
+    PropertyBindingComponent,
+    EventBindingComponent,
+    TwoWayDataBindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ data-binding-demo/src/app/string-interpolation/string-interpolation.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-string-interpolation',
+  templateUrl: './string-interpolation.component.html',
+  styleUrls: ['./string-interpolation.component.css']
+})
+export class StringInterpolationComponent {
+  status: String = 'Active';
+  id: number = 1;
+}
